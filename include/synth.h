fa3cfc8629078e8ebbe131925a1dc5a18c29a67d
Aaron Thompson <athompsonbriley.56@gmail.com>
2024-11-13 22:28:59 -0500

    Updated Code

--- include/synth.h
+++ include/synth.h
@@ -4,7 +4,8 @@
 #include "types.h"
 
 namespace Synth {
-    void SYNTH_API delay(size_t secends);
+  void SYNTH_API delay(size_t secends);
+  void SYNTH_API println(const char* format, ...);
 }
 
 #endif //SYNTH_H
--- src/synth.cpp
+++ src/synth.cpp
@@ -1,9 +1,20 @@
+#include <cstdio>
+#include <cstdarg>
 #include <unistd.h>
 
 #include "../include/synth.h"
 
 namespace Synth {
-    void delay(size_t secends) {
+   void delay(size_t secends) {
         sleep(secends);
-    }
+   }
+
+  void println(const char* format, ...) {
+    std::va_list args;
+    va_start(args, format);
+
+    std::vprintf(format, args);
+    
+    va_end(args);
+  }
 }
