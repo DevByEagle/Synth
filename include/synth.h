dc2327939e092efd3cadecb85b1062be804bf340
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2024-11-14 18:16:36 -0500

    Update synth.h

--- include/synth.h
+++ include/synth.h
@@ -1,11 +1,28 @@
 #ifndef SYNTH_H
 #define SYNTH_H
 
-#include "types.h"
+#if defined(_WIN32)
+    #ifdef EXPORTS
+        #define SYNAPI __declspec(dllexport)
+    #else
+        #define SYNAPI __declspec(dllimport)
+    #endif
+#elif defined(__linux__) || defined(__APPLE__)
+    #define SYNAPI __attribute__((visibility("default")))
+#else
+    #error "Synth is not supported"
+#endif
+
+#include "exception.h"
 
 namespace Synth {
-  void SYNTH_API delay(size_t secends);
-  void SYNTH_API println(const char* format, ...);
+    // Types
+    typedef unsigned int size_t;
+
+    // Functions 
+    void SYNAPI delay(size_t secends);
+    
+    // Classes
 }
 
-#endif //SYNTH_H
+#endif // SYNTH_H
